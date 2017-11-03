feb64c4686dccc80f127934217f3e1b5387ddfb4
rofrischmann <robin@rofrischmann.de>
2017-11-04 00:33:36 +0100

    remove bundle files

--- modules/generateLockFilePlugin.js
+++ modules/generateLockFilePlugin.js
@@ -1,90 +1,96 @@
-const fs = require("fs");
+const fs = require('fs')
 
 const typeAlias = {
-  StringTypeAnnotation: "String",
-  StringLiteral: "String",
-  NumberTypeAnnotation: "Number",
-  NumericLiteral: "Number",
-  ArrayExpression: "Array",
-  BooleanLiteral: "Boolean"
-};
+  StringTypeAnnotation: 'String',
+  StringLiteral: 'String',
+  NumberTypeAnnotation: 'Number',
+  NumericLiteral: 'Number',
+  ArrayExpression: 'Array',
+  BooleanLiteral: 'Boolean'
+}
+
+const MISSING_TYPE = 'MISSING_TYPE'
 
 module.exports = function() {
-  const typeFile = {};
+  const typeFile = {}
+  let fileName = ''
 
   const functionTraverse = (path, state) => {
-    const paramTypes = path.node.params.map(
-      param => typeAlias[param.typeAnnotation.typeAnnotation.type]
-    );
+    const functionName = path.node.id ? path.node.id.name : path.node.key.name
 
-    let returnType = path.node.returnType.typeAnnotation.type;
+    const paramTypes = path.node.params.map(param => {
+      if (param.typeAnnotation) {
+        return typeAlias[param.typeAnnotation.typeAnnotation.type]
+      } else {
+        console.log(
+          `Parameter "${param.name}" in "${functionName}" (${fileName}) is missing type annotations.`
+        )
+        return MISSING_TYPE
+      }
+    })
+
+    let returnType = path.node.returnType.typeAnnotation.type
 
     const shape = {
-      name: path.node.id ? path.node.id.name : path.node.key.name,
+      name: functionName,
       params: paramTypes
-    };
+    }
 
-    if (returnType === "GenericTypeAnnotation") {
-      const innerTypes = {};
+    if (returnType === 'GenericTypeAnnotation') {
+      const innerTypes = {}
 
       path.traverse({
         ReturnStatement(returnPath) {
           returnPath.traverse({
             ObjectProperty(propPath) {
               innerTypes[propPath.node.key.name] =
-                typeAlias[propPath.node.value.type];
+                typeAlias[propPath.node.value.type]
             },
             ObjectMethod(methPath) {
-              innerTypes[methPath.node.key.name] = functionTraverse(methPath);
+              innerTypes[methPath.node.key.name] = functionTraverse(methPath)
             }
-          });
+          })
         }
-      });
+      })
 
-      shape.return = innerTypes;
+      shape.return = innerTypes
     } else {
-      shape.return = typeAlias[returnType];
+      shape.return = typeAlias[returnType]
     }
 
-    return shape;
-  };
+    return shape
+  }
   return {
-    inherits: require("babel-plugin-syntax-flow"),
+    inherits: require('babel-plugin-syntax-flow'),
 
     visitor: {
       Program: {
         enter(path, state) {
-          const relativeFilename = state.file.opts.sourceFileName;
+          fileName = state.file.opts.sourceFileName
 
-          if (!typeFile[relativeFilename]) {
-            typeFile[relativeFilename] = [];
+          if (!typeFile[fileName]) {
+            typeFile[fileName] = []
           }
         },
         exit(path, state) {
-          const relativeFilename = state.file.opts.sourceFileName;
-
-          if (Object.keys(typeFile[relativeFilename]).length === 0) {
-            delete typeFile[relativeFilename];
+          if (Object.keys(typeFile[fileName]).length === 0) {
+            delete typeFile[fileName]
           }
 
           fs.writeFile(
-            ".types-lock.json",
+            '.types-lock.json',
             JSON.stringify(typeFile, null, 2),
             err => {
-              if (err) throw err;
+              if (err) throw err
 
-              console.log(
-                "Types for " + relativeFilename + " added to .types-lock.json."
-              );
+              console.log(`Types for ${fileName} added to .types-lock.json.`)
             }
-          );
+          )
         }
       },
       FunctionDeclaration(path, state) {
-        const relativeFilename = state.file.opts.sourceFileName;
-
-        typeFile[relativeFilename].push(functionTraverse(path, state));
+        typeFile[fileName].push(functionTraverse(path))
       }
     }
-  };
-};
+  }
+}
--- test/src/bums.js
+++ test/src/bums.js
@@ -1,19 +1,19 @@
 /* @flow */
-export default function bums(num: number): Object {
+export default function bums(num: number, foo): Object {
   return {
-    color: "red",
+    color: 'red',
     bar(a: string, b: number): boolean {
-      return b > 0;
+      return b > 0
     },
     baz(): Object {
       return {
         size: 1,
-        bar: ["1", "2", "4"],
+        bar: ['1', '2', '4'],
         balo(c: string): number {
-          return 5;
+          return 5
         }
-      };
+      }
     },
     foo: true
-  };
+  }
 }
